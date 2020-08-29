6908ceb2c25a0d4db8a466666985313ad28c8ecd
Denny Afrizal <denny.afrizal@alfadigital.id>
2020-08-30 01:18:29 +0700

    Login Page Function

--- src/main/java/com/mitrais/controller/PageController.java
+++ src/main/java/com/mitrais/controller/PageController.java
@@ -3,9 +3,6 @@ package com.mitrais.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 @RequestMapping("/page")
 public class PageController {
